4f3a815467ec66b3a97bcbe7753e62d4fd358947
mrdcvlsc <mrdcvlsc@gmail.com>
2024-03-17 10:13:14 +0800

    Lab 2

--- main.c
+++ main.c
@@ -1,74 +1,102 @@
-#include <math.h>
-#include <stdio.h>
+// #include <conio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
 
-// #include <conio.h> - not supported by the compiler in my system
+void normalize(float *, int *);
+void ufof(float, int);
 
-int main() {
-    int decimal = 0, binary = 0, choice, rem, temp_number, i, number;
-    while (1) {
-        // clrscr(); - not supported by the compiler in my system
-        printf("\n\t\t\tBinary To Decimal And Decimal To Binary");
-        printf("\n\t\t\t---------------------------------------");
-        printf("\n1) Binary to decimal");
-        printf("\n2) Decimal to binary");
-        printf("\n3) Exit");
-
-        printf("\nEnter your Choice : ");
-        scanf("%d", &choice);
-        switch (choice) {
-            case 1: {
-                i = 0;
-                printf("\nEnter the binary number : ");
-                scanf("%d", &binary);
-                number = binary;
-                while (number != 0) {
-                    rem = number % 10;
-                    number = number / 10;
-                    if (!(rem == 1 || rem == 0)) {
-                        printf("\nInvalid data");
-                        goto z;
-                    }
-                }
-                rem = 0;
-                temp_number = binary;
-                while (temp_number != 0) {
-                    rem = temp_number % 10;
-                    temp_number /= 10;
-                    decimal += rem * pow(2, i);
-                    i++;
-                }
-                printf("\nThe Binary number is \"%d\"\nThe  Decimal number is \"%d\"\n", binary, decimal);
-                break;
+void main() {
+    // clrscr();
+    float x, x1, x2;
+    int   e, e1, e2, k, choice;
+    printf("\n1) add");
+    printf("\n2) sub");
+    printf("\n3) mul");
+    printf("\n4) div");
+    printf("\nenter ur choice: ");
+    scanf("%d", &choice);
+    printf("\nenter the 1st mantissa: ");
+    scanf("%f", &x1);
+    printf("enter the 1nd exponent: ");
+    scanf("%d", &e1);
+    printf("\nenter the 2nd mantissa: ");
+    scanf("%f", &x2);
+    printf("enter the 2nd exponent: ");
+    scanf("%d", &e2);
+    switch (choice) {
+        case 1: {
+            k = e1 - e2;
+            if (e1 > e2) {
+                x2 = x2 / pow(10, k);
+                e = e1;
+            } else {
+                x1 = x1 / pow(10, k);
+                e = e2;
             }
-
-            case 2: {
-                i = 1;
-                printf("\nEnter the decimal number : ");
-                scanf("%d", &decimal);
-                rem = 0;
-                temp_number = decimal;
-                while (temp_number != 0) {
-                    rem = temp_number % 2;
-                    temp_number /= 2;
-                    binary += rem * i;
-                    i *= 10;
-                }
-                printf("\nThe Decimal number is \"%d\"\nThe Binary  number is \"%d\"\n", decimal, binary);
-                break;
-            }
-            case 3: {
-                exit(0);
-            }
-            default: {
-                printf("\nEnter the proper choice");
-            }
+            x = x1 + x2;
+            normalize(&x, &e);
+            ufof(x, e);
+            break;
         }
-    z:
-        decimal = binary = temp_number = 0;
-
-        // getch(); - not supported by the compiler in my system
+        case 2: {
+            k = e1 - e2;
+            if (e1 > e2) {
+                x2 = x2 / pow(10, k);
+                e = e1;
+            } else {
+                x1 = x1 / pow(10, k);
+                e = e2;
+            }
+            x = x1 - x2;
+            normalize(&x, &e);
+            ufof(x, e);
+            break;
+        }
+        case 3: {
+            x = x1 * x2;
+            e = e1 + e2;
+            normalize(&x, &e);
+            ufof(x, e);
+            break;
+        }
+        case 4: {
+            x = x1 / x2;
+            e = e1 - e2;
+            normalize(&x, &e);
+            ufof(x, e);
+            break;
+        }
+        default: {
+            printf("\nenter the proper choice");
+            break;
+        }
     }
+    
+    getchar();
+    getchar();
+}
 
-    return 0;
+void normalize(float *x, int *e) {
+
+    while (*x < 0.1) {
+        *x *= 10;
+        *e -= 1;
+    }
+    while (*x >= 1.0) {
+        *x /= 10;
+        *e += 1;
+    }
+}
+
+void ufof(float x, int e) {
+    if (e > 99) {
+        printf("\nThe ans is mantissa=%.4f, exponent=%d\n", x, e);
+        printf("\nover flow error");
+    } else if (e < -99) {
+        printf("\nThe ans is mantissa =%.4f, exponent=%d\n", x, e);
+        printf("\nunder flow error");
+    } else {
+        printf("\nThe ans is mantissa=%.4f, exponent=%d\n", x, e);
+    }
 }
